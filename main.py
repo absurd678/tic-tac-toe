f3e9eef53e62eee24b6e3e187a5df69ca042d317
absurd678 <47460087+absurd678@users.noreply.github.com>
2021-01-11 21:07:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# Welcome to this strange version of tic-tac-toe game!
+# The rules here are so oversimplified - 
+# the human moves first, as he has "X",
+# the computer moves after, it has "O".
+# To simplify the understanding of method's role, 
+# I'll introduce you definition of marks here:
+# ! - Means that it is used in main cycle
+# (method's name) - Means that it is called inside the method 
+
 from abc import ABC
 import game
 class Players(ABC):
     def __init__(self):
         self.moves = []
-        self.order = 0
     def __getitem__(self, item):
         return self.moves[item]
     def __len__(self):
@@ -13,27 +21,27 @@ class Human(Players):
     def __init__(self):
         super().__init__()
         self.sign = "X"
-    def choose_move(self):
+    def choose_move(self):  # (.ask_move())
         move = game.ask_question("Choose your next move's index: ", 0, 9)
         return move
-    def win(self):
+    def win(self):  # !
         print("____COMPUTER____")
         print("NOOOOOOOO WHY SILLY HUMAN HAS WON???!")
-    def add(self, move):
+    def add(self, move):  # (.ask_move())
         self.moves.append(move)
 
 class Computer(Players):
     def __init__(self):
         super().__init__()
         self.sign = "O"
-    def choose_move(self):
+    def choose_move(self):  # (.ask_move())
         import random
         move = random.randrange(0, 9)
         return move
-    def win(self):
+    def win(self):  # !
         print("____COMPUTER____")
         print("HAHAHAHHAHAHHAHAHHHAHAHAHAHAH I BEAT YOU MAN")
-    def add(self, move):
+    def add(self, move):  # (.ask_move())
         print("____COMPUTER____")
         print("Now it's my turn!")
         self.moves.append(move)
@@ -48,7 +56,7 @@ class Board:
     def __setitem__(self, key, value):
         self.board[key] = value
         return self.board
-    def show_board(self):
+    def show_board(self):  # !
         print(f"{self.board[0]} | {self.board[1]} | {self.board[2]}", end="\n")
         print("----------")
         print(f"{self.board[3]} | {self.board[4]} | {self.board[5]}", end="\n")
@@ -74,7 +82,7 @@ class Game:
         self.results = []
         self.squares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    def ask_move(self):  # (!)
+    def ask_move(self):  # !
         move = None
         while move not in self.squares:  # If the selected square has already been an str
             move = self.players[0].choose_move()
@@ -83,13 +91,13 @@ class Game:
         self.change_board(self.players[0])
         self.change_combinations(move)
 
-    def change_board(self, player):
+    def change_board(self, player):  # (.ask_move())
         for move in range(len(player.moves)):
             for number in range(len(self.table.board)):  # Because it is a tender method, we define .board, as it is obj
                 if player.moves[move] == self.table.board[number]:
                     self.table.board[number] = player.sign
 
-    def change_combinations(self, move):  # ()  As it was, nothing changed
+    def change_combinations(self, move):  # (.ask_move())
         player = self.players[0]
         for round in range(len(self.combinations)):  # checking parentheses
             for number in range(len(self.combinations[round])):
@@ -99,7 +107,7 @@ class Game:
     def checking_three(self, player):  # !
         result = False
         for list in self.combinations:
-            for num in list:  # Checks every list, if there aren't three X or O, then returns False
+            for num in list:  
                 if num == player.sign:
                     self.results.append(num)
             if len(self.results) == 3:
@@ -115,7 +123,7 @@ class Game:
     def order(self):  # !
         self.players.reverse()
 
-    def game(self):
+    def game(self):  # Here is a cycle 
         victory = "Draft"  # The initial, and if the .checking_three returned nothing
         while not self.squares_ended():
             print()
